7eb89f727b14fd55fe0ac0cdbfb8788f6f806d29
Tae-co <dcnfsjfzp2@privaterelay.appleid.com>
2025-12-03 17:38:12 +0900

    tomcat v10

--- src/main/java/com/shoppingmallcoco/project/ShoppingmallCocoApplication.java
+++ src/main/java/com/shoppingmallcoco/project/ShoppingmallCocoApplication.java
@@ -2,13 +2,28 @@ package com.shoppingmallcoco.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.WebApplicationContext;
 
 @EnableJpaAuditing
-@SpringBootApplication
-public class ShoppingmallCocoApplication {
+@SpringBootApplication(scanBasePackages = "com.shoppingmallcoco.project")
+public class ShoppingmallCocoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingmallCocoApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShoppingmallCocoApplication.class);
+    }
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(ShoppingmallCocoApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
